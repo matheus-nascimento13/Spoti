6c2dcd3bea06812e44dbc473ee0ec312fc9d4789
matheus-nascimento13 <mt.pereira@hotmail.com>
2023-09-08 09:58:06 -0300

    new

--- main.js/main.js
+++ main.js/main.js
@@ -1,7 +1,11 @@
-const addplaylist = [...document.querySelectorAll(".add")]
-const cards = [...document.querySelectorAll(".card")]
-const trashIcon = [...document.querySelectorAll(".trash")]
-const caixa = document.querySelector(".caixa")
+const addplaylist = [...document.querySelectorAll(".add")];
+const cards = [...document.querySelectorAll(".card")];
+const trashIcon = [...document.querySelectorAll(".trash")];
+const caixa = document.querySelector(".caixa");
+const styleBottom = [...document.querySelectorAll(".b-style")];
+const box_2 = document.querySelector(".caixa_2")
+
+
 
 
 
@@ -11,21 +15,43 @@ addplaylist.map((el, i) => {
         console.log(el, i);
         let elementoNovo = document.createElement('div');
         elementoNovo = cards[i];
-        caixa.appendChild(elementoNovo)
+        caixa.appendChild(elementoNovo);
     })
 })
 
 trashIcon.forEach((ele, ind) => {
-    console.log(ele, ind)
+    console.log(ele, ind);
     ele.addEventListener("click", () => {
-        const selectionMusic = cards[ind]
+        const selectionMusic = cards[ind];
         selectionMusic.remove()
     })
 
 })
 
-// addplaylist.addEventListener("click", () => {
-//     let elementoNovo = document.createElement('div');
-//     elementoNovo = cards[0]
-//     caixa.appendChild(elementoNovo)
-// })
+
+styleBottom.map((element, i) => {
+
+    element.addEventListener("click", () => {
+        console.log(element.textContent);
+        const styleMusic = [...document.querySelectorAll(".style")];
+        const result = styleMusic.filter((styleMusic) => {
+            if (styleMusic.textContent == element.textContent) {
+
+            } else {
+                let elementFilter = document.createElement('div');
+                elementFilter = cards[i]
+                box_2.appendChild(elementFilter)
+            }
+
+
+
+        })
+
+    })
+})
+
+
+
+
+
+
